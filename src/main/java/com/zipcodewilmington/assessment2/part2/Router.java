2327edcf4378612018fc548e5abc2d76bb0d67bc
Jason Gibbs <jason_r_gibbs@yahoo.com>
2018-06-18 12:38:48 -0400

    ArrayUtility Only filing test

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,4 +1,48 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import java.util.Set;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.TreeMap;
+
 public class Router {
+
+    private  Map<String, String> routerMap;
+
+    public Router() {
+        routerMap = new TreeMap<>();
+    }
+
+    public void add(String path, String controller) {
+        routerMap.put(path, controller);
+    }
+
+    public int size() {
+        return routerMap.size();
+    }
+
+    public String getController(String path) {
+        String controllerFound = routerMap.get(path);
+        return controllerFound;
+    }
+
+    public void update(String path, String newController) {
+        routerMap.get(path);
+        routerMap.put(path, newController);
+    }
+
+
+    public void remove(String path) {
+        routerMap.remove(path);
+    }
+
+    @Override
+    public String toString() {
+        String describe = "";
+        Set<Map.Entry<String, String>> setOfMap = routerMap.entrySet();
+        for(Map.Entry<String, String> entry : setOfMap) {
+            describe += entry.getKey() + " -> " + entry.getValue() + "\n";
+        }
+        return describe;
+    }
 }
