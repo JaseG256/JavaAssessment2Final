e7188b211e20612391bde729bd2635fd291d1de2
Jason Gibbs <jason_r_gibbs@yahoo.com>
2018-06-18 09:35:25 -0400

    Part 1 - all tests passed#

--- src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
+++ src/main/java/com/zipcodewilmington/assessment2/part1/WuTangConcatenator.java
@@ -1,4 +1,32 @@
 package com.zipcodewilmington.assessment2.part1;
 
 public class WuTangConcatenator {
+
+    private boolean isWu;
+    private boolean isTang;
+    private boolean isWuTang;
+    private Integer input;
+
+    public WuTangConcatenator(Integer input) {
+        this.input = input;
+    }
+
+
+    public boolean isWu() {
+        isWu = false;
+        isWu = (input % 3 == 0) ? true : false;
+        return isWu;
+    }
+
+    public boolean isTang() {
+        isTang = false;
+        isTang = (input % 5 == 0) ? true : false;
+        return isTang;
+    }
+
+    public boolean isWuTang() {
+        isWuTang = false;
+        isWuTang = (input % 5 == 0 && input % 3 == 0) ? true : false;
+        return isWuTang;
+    }
 }
